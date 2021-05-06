abc47c2671e6810f7a19fb128a3b7f1ba6fb43f5
Paulo Valente <polvalente@gmail.com>
2021-05-06 01:29:42 -0300

    fix: minor improvements (#1)
    * chore: use if/else for boolean case
    
    * fix: don't add credo and ex_doc as dependencies for client libs
    
    * test: add tests for multiplicity assertion
    
    * chore: add named functions and minor improvements to copeiro.ex
    
    * chore: downcase variable names

--- lib/copeiro.ex
+++ lib/copeiro.ex
@@ -3,89 +3,14 @@ defmodule Copeiro do
   The Copeiro package provides assertion functions that will enhance your testing experience in Elixir
   """
 
-  @doc false
-  def __assert_lists__(:==, left, right, any_order: true) do
-    left
-    |> Copeiro.Comparator.match_lists_in_any_order(right)
-    |> case do
-      {:error, _, _} ->
-        ExUnit.Assertions.flunk("""
-        assertion failed, lists does not match
-        left: #{inspect(left)}
-        right: #{inspect(right)}
-        """)
-
-      :ok ->
-        true
-    end
-  end
-
-  def __assert_lists__(:==, left, right, _opts) do
-    case left == right do
-      false ->
-        ExUnit.Assertions.flunk("""
-        Comparison (using ==) failed in:
-        left: #{inspect(left)}
-        right: #{inspect(right)}
-        """)
-
-      _ ->
-        true
-    end
-  end
-
-  def __assert_lists__(:in, left, right, _opts) do
-    left
-    |> Enum.reduce_while(true, fn l, _ ->
-      case l in right do
-        true -> {:cont, true}
-        false -> {:halt, {:error, l}}
-      end
-    end)
-    |> case do
-      {:error, value} ->
-        ExUnit.Assertions.flunk("""
-        assertion failed, value not found
-        value: #{inspect(value)}
-        left: #{inspect(left)}
-        right: #{inspect(right)}
-        """)
-
-      _ ->
-        true
-    end
-  end
-
-  def __assert_lists__(:not_in, left, right, _opts) do
-    left
-    |> Enum.reduce_while(true, fn l, _ ->
-      case l not in right do
-        true -> {:cont, true}
-        false -> {:halt, {:error, l}}
-      end
-    end)
-    |> case do
-      {:error, value} ->
-        ExUnit.Assertions.flunk("""
-        match succeeded, but should have failed
-        value: #{inspect(value)}
-        left: #{inspect(left)}
-        right: #{inspect(right)}
-        """)
-
-      _ ->
-        true
-    end
-  end
-
   @doc """
   Asserts that two lists matches
 
   ## Examples
 
-  For the following examples`LEFT` and `RIGHT` will be used to describe the expression `assert_lists LEFT OPERATOR RIGHT`
+  For the following examples `left` and `right` will be used to describe the expression `assert_lists left OPERATOR right`
 
-  ### All elements of `LEFT` are also elements of `RIGHT`
+  ### All elements of `left` are also elements of `right`
 
     ```
     iex> assert_lists [1, 2] in [0, 2, 1, 3]
@@ -95,7 +20,7 @@ defmodule Copeiro do
     true
     ```
 
-  ### `LEFT` and `RIGHT` has no element in common
+  ### `left` and `right` has no element in common
 
     ```
     iex> assert_lists [1, 2] not in [3, 4]
@@ -132,20 +57,97 @@ defmodule Copeiro do
         {op, _, [left, right]} -> {op, left, right}
       end
 
-    quote bind_quoted: [op: op, left: left, right: right, opts: opts] do
-      [left, right, opts] =
-        (fn left, right, opts ->
-           keys = Keyword.get(opts, :keys, [])
-
-           if keys == [] do
-             [left, right, opts]
-           else
-             t = fn lst -> Enum.map(lst, &Map.take(&1, keys)) end
-             [t.(left), t.(right), Keyword.delete(opts, :keys)]
-           end
-         end).(left, right, opts)
+    quote bind_quoted: [op: op, left: left, right: right, opts: opts], location: :keep do
+      {left, right, opts} = Copeiro.__access_keys__(left, right, opts)
 
       Copeiro.__assert_lists__(op, left, right, opts)
     end
   end
+
+  @doc false
+  def __assert_lists__(:==, left, right, any_order: true) do
+    left
+    |> Copeiro.Comparator.match_lists_in_any_order(right)
+    |> case do
+      {:error, _, _} ->
+        ExUnit.Assertions.flunk("""
+        assertion failed, lists does not match
+        left: #{inspect(left)}
+        right: #{inspect(right)}
+        """)
+
+      :ok ->
+        true
+    end
+  end
+
+  def __assert_lists__(:==, left, right, _opts) do
+    if left == right do
+      true
+    else
+      ExUnit.Assertions.flunk("""
+      Comparison (using ==) failed in:
+      left: #{inspect(left)}
+      right: #{inspect(right)}
+      """)
+    end
+  end
+
+  def __assert_lists__(:in, left, right, _opts) do
+    left
+    |> Enum.reduce_while(:ok, fn l, _ ->
+      if l in right do
+        {:cont, :ok}
+      else
+        {:halt, {:error, l}}
+      end
+    end)
+    |> case do
+      {:error, value} ->
+        ExUnit.Assertions.flunk("""
+        assertion failed, value not found
+        value: #{inspect(value)}
+        left: #{inspect(left)}
+        right: #{inspect(right)}
+        """)
+
+      :ok ->
+        true
+    end
+  end
+
+  def __assert_lists__(:not_in, left, right, _opts) do
+    left
+    |> Enum.reduce_while(true, fn l, _ ->
+      case l not in right do
+        true -> {:cont, true}
+        false -> {:halt, {:error, l}}
+      end
+    end)
+    |> case do
+      {:error, value} ->
+        ExUnit.Assertions.flunk("""
+        match succeeded, but should have failed
+        value: #{inspect(value)}
+        left: #{inspect(left)}
+        right: #{inspect(right)}
+        """)
+
+      _ ->
+        true
+    end
+  end
+
+  @doc false
+  def __access_keys__(left, right, opts) do
+    {keys, opts} = Keyword.pop(opts, :keys, [])
+
+    if keys == [] do
+      {left, right, opts}
+    else
+      {do_access_keys(left, keys), do_access_keys(right, keys), opts}
+    end
+  end
+
+  defp do_access_keys(lst, keys), do: Enum.map(lst, &Map.take(&1, keys))
 end
